cec0c5f605aa77b4581cd3338164773757f3d054
msmyslov666 <msmyslov666@gmail.com>
2019-07-18 23:22:37 +0300

    minor correction

--- __tests__/test.test.js
+++ __tests__/test.test.js
@@ -1,13 +1,6 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-const obj = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
-};
-
 const text = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'UTF-8');
 
 const before = `${__dirname}/__fixtures__/before.json`;
